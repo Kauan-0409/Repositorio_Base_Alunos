1ad47576e6eb9f95ebe5c4326b64c69b72b3b4cf
Kauan// <kauandosantostes@gmail.com>
2026-05-20 11:15:53 -0300

    Add files via upload

---
+++ Modulo 2/Manipulação de Arquivos e Automação/automacao.py
@@ -0,0 +1,27 @@
+import pyautogui as pg
+
+pg.moveTo(1000, 1050, duration=0.5)
+
+pg.click()
+
+pg.moveTo(200, 60, duration=0.5)
+
+pg.write("youtube", interval=0.3)
+
+pg.press("enter")
+
+pg.sleep(1.7)
+
+pg.moveTo(700, 160, duration=0.5)
+
+pg.click()
+
+pg.write("manual do mundo", interval=0.3)
+
+pg.press("enter")
+
+pg.sleep(2.5)
+
+pg.moveTo(629,908, duration=0.5)
+
+pg.click()
